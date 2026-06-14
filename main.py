22e273e5550cf03ac7da25712ccf9620767315a6
Kevin Lyter <code@lyterk.com>
2026-06-14 15:53:32 -0700

    Output file from input

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from src.spanishdict import process_file
 
 if __name__ == "__main__":
     input_file = sys.argv[1]
-    output_file = sys.argv[2]
+    output_file = f"{input_file}.out"
     asyncio.run(process_file(input_file, output_file))
